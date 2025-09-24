86482a68b19d88e9579c6a1661a816da50ccd35e
Arturo Laurenzi <arturo.laurenzi@iit.it>
2025-09-24 12:55:09 +0200

    wip

--- source/kyon_isaac/kyon_isaac/env/manager_based_xbot2_env.py
+++ source/kyon_isaac/kyon_isaac/env/manager_based_xbot2_env.py
@@ -1,5 +1,9 @@
 from isaaclab.envs import ManagerBasedEnv, ManagerBasedEnvCfg
 from isaaclab.managers import ObservationManager, ActionManager, CommandManager
+from isaaclab.sensors.contact_sensor import ContactSensorCfg, ContactSensorData
+from isaaclab.sensors.imu import ImuCfg, ImuData
+from isaaclab.scene.interactive_scene_cfg import InteractiveSceneCfg
+from isaaclab.assets.articulation import ArticulationCfg
 from isaaclab.utils import configclass
 import torch
 from typing import Sequence
@@ -17,14 +21,16 @@ class XBot2RobotData:
         self.applied_torque = torch.zeros((1, 12))
 
 class XBot2Robot:
-    data: XBot2RobotData = XBot2RobotData() 
-    joint_names: list[str] = [
-        'hip_roll_1', 'hip_pitch_1', 'knee_pitch_1',
-        'hip_roll_2', 'hip_pitch_2', 'knee_pitch_2',
-        'hip_roll_3', 'hip_pitch_3', 'knee_pitch_3',
-        'hip_roll_4', 'hip_pitch_4', 'knee_pitch_4'
-    ]
-    num_joints: int = len(joint_names)
+    def __init__(self, cfg: ArticulationCfg):
+        self.cfg = cfg
+        self.data: XBot2RobotData = XBot2RobotData()
+        self.joint_names: list[str] = [
+            'hip_roll_1', 'hip_pitch_1', 'knee_pitch_1',
+            'hip_roll_2', 'hip_pitch_2', 'knee_pitch_2',
+            'hip_roll_3', 'hip_pitch_3', 'knee_pitch_3',
+            'hip_roll_4', 'hip_pitch_4', 'knee_pitch_4'
+            ]
+        self.num_joints: int = len(self.joint_names)
     def find_joints(self, name_keys: str | Sequence[str], joint_subset: list[str] | None = None, preserve_order: bool = False
     ) -> tuple[list[int], list[str]]:
         if joint_subset is None:
@@ -32,23 +38,54 @@ class XBot2Robot:
         # find joints
         return string_utils.resolve_matching_names(name_keys, joint_subset, preserve_order)
 
-@configclass
-class XBot2ImuSensorData:
-    ang_vel_b: torch.Tensor = torch.zeros((1, 3))
-    lin_acc_b: torch.Tensor = torch.zeros((1, 3))
     
 class XBot2ImuSensor:
-    data: XBot2ImuSensorData = XBot2ImuSensorData()
+    def __init__(self, cfg: ImuCfg):
+        self.cfg = cfg
+        self.data = ImuData()
+        self.data.lin_acc_b = torch.zeros((1, 3))
+        self.data.ang_vel_b = torch.zeros((1, 3))
 
-@configclass
+    
+class XBot2ContactSensor:
+    
+    def __init__(self, cfg: ContactSensorCfg):
+        self.cfg = cfg
+        self.data = ContactSensorData()
+        self.body_names: list[str] = ['contact_1', 'contact_2', 'contact_3', 'contact_4']
+        self.num_bodies: int = len(self.body_names)
+        self.data.net_forces_w_history = torch.zeros((1, self.cfg.history_length, self.num_bodies, 3))
+    
+    def find_bodies(self, name_keys: str | Sequence[str], preserve_order: bool = False) -> tuple[list[int], list[str]]:
+        return string_utils.resolve_matching_names(name_keys, self.body_names, preserve_order)
+    
+    
 class XBot2Scene:
-    robot: XBot2Robot = XBot2Robot()
-    imu_sensor: XBot2ImuSensor = XBot2ImuSensor()
-    num_envs: int = 1
+    
+    def __init__(self, cfg: InteractiveSceneCfg):
+        self.cfg = cfg
+        self._assets = dict()
+        self.sensors = dict()
+        self.num_envs = 1
+        for asset_name, asset_cfg in self.cfg.__dict__.items():
+            # skip keywords
+            if asset_name in InteractiveSceneCfg.__dataclass_fields__ or asset_cfg is None:
+                continue
+            
+            print(f'Loading asset: {asset_name} type {type(asset_cfg)}')
+            if isinstance(asset_cfg, ArticulationCfg):
+                self._assets[asset_name] = XBot2Robot(asset_cfg)
+            elif isinstance(asset_cfg, ImuCfg):
+                self._assets[asset_name] = XBot2ImuSensor(asset_cfg)
+                self.sensors[asset_name] = self._assets[asset_name]
+            elif isinstance(asset_cfg, ContactSensorCfg):
+                self._assets[asset_name] = XBot2ContactSensor(asset_cfg)
+                self.sensors[asset_name] = self._assets[asset_name]
+            
     def __getitem__(self, key: str):
-        return getattr(self, key)
+        return self._assets[key]
     def keys(self):
-        return self.__dict__.keys()
+        return self._assets.keys()
 
 class SimMockup:
     def __init__(self):
@@ -62,7 +99,7 @@ class ManagerBasedXBot2Env(ManagerBasedEnv):
     def __init__(self, cfg: ManagerBasedEnvCfg):
         
         self.sim = SimMockup()  # Placeholder for actual simulation initialization
-        self.scene = XBot2Scene()  # Placeholder for actual scene initialization
+        self.scene = XBot2Scene(cfg.scene)  # Placeholder for actual scene initialization
         self.action_manager = ActionManager(cfg=cfg.actions, env=self)
         self.command_manager = CommandManager(cfg=cfg.commands, env=self) 
         self.observation_manager = ObservationManager(cfg=cfg.observations, env=self)
