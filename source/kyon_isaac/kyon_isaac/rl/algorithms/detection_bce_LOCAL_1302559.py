d1fe062f56b42df469a2c2adcd9aa5c4e41776d5
Luca Rossini <luca.rossini@iit.it>
2026-04-15 16:07:22 +0000

    merge

---
+++ source/kyon_isaac/kyon_isaac/rl/algorithms/detection_bce_LOCAL_1302559.py
@@ -0,0 +1,334 @@
+# Copyright (c) 2021-2025, ETH Zurich and NVIDIA CORPORATION
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+from __future__ import annotations
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from itertools import chain
+from tensordict import TensorDict
+
+from rsl_rl.modules import ActorCritic, ActorCriticRecurrent
+from rsl_rl.modules.rnd import RandomNetworkDistillation
+from rsl_rl.storage import RolloutStorage
+from rsl_rl.utils import string_to_callable
+
+from kyon_isaac.rl.modules import ActorCriticWithCNN
+
+class Transition(RolloutStorage.Transition):
+    latent: torch.Tensor | None = None
+
+class DetectionBCE:
+    """Proximal Policy Optimization algorithm (https://arxiv.org/abs/1707.06347)."""
+
+    policy: ActorCriticWithCNN
+    """The actor critic module."""
+
+    def __init__(
+        self,
+        policy: ActorCriticWithCNN,
+        num_learning_epochs: int = 5,
+        num_mini_batches: int = 4,
+        clip_param: float = 0.2,
+        gamma: float = 0.99,
+        lam: float = 0.95,
+        value_loss_coef: float = 1.0,
+        entropy_coef: float = 0.01,
+        latent_coef: float = 1.,
+        learning_rate: float = 0.001,
+        max_grad_norm: float = 1.0,
+        use_clipped_value_loss: bool = True,
+        schedule: str = "adaptive",
+        desired_kl: float = 0.01,
+        loss_type: str = "mse",
+        device: str = "cpu",
+        normalize_advantage_per_mini_batch: bool = False,
+        # RND parameters
+        rnd_cfg: dict | None = None,
+        # Symmetry parameters
+        symmetry_cfg: dict | None = None,
+        # Distributed training parameters
+        multi_gpu_cfg: dict | None = None,
+    ) -> None:
+        # Device-related parameters
+        self.device = device
+        self.is_multi_gpu = multi_gpu_cfg is not None
+
+        # Multi-GPU parameters
+        if multi_gpu_cfg is not None:
+            self.gpu_global_rank = multi_gpu_cfg["global_rank"]
+            self.gpu_world_size = multi_gpu_cfg["world_size"]
+        else:
+            self.gpu_global_rank = 0
+            self.gpu_world_size = 1
+
+        # RND components
+        if rnd_cfg is not None:
+            # Extract parameters used in ppo
+            rnd_lr = rnd_cfg.pop("learning_rate", 1e-3)
+            # Create RND module
+            self.rnd = RandomNetworkDistillation(device=self.device, **rnd_cfg)
+            # Create RND optimizer
+            params = self.rnd.predictor.parameters()
+            self.rnd_optimizer = optim.Adam(params, lr=rnd_lr)
+        else:
+            self.rnd = None
+            self.rnd_optimizer = None
+
+        # Symmetry components
+        if symmetry_cfg is not None:
+            # Check if symmetry is enabled
+            use_symmetry = symmetry_cfg["use_data_augmentation"] or symmetry_cfg["use_mirror_loss"]
+            # Print that we are not using symmetry
+            if not use_symmetry:
+                print("Symmetry not used for learning. We will use it for logging instead.")
+            # If function is a string then resolve it to a function
+            if isinstance(symmetry_cfg["data_augmentation_func"], str):
+                symmetry_cfg["data_augmentation_func"] = string_to_callable(symmetry_cfg["data_augmentation_func"])
+            # Check valid configuration
+            if not callable(symmetry_cfg["data_augmentation_func"]):
+                raise ValueError(
+                    f"Symmetry configuration exists but the function is not callable: "
+                    f"{symmetry_cfg['data_augmentation_func']}"
+                )
+            # Check if the policy is compatible with symmetry
+            if isinstance(policy, ActorCriticRecurrent):
+                raise ValueError("Symmetry augmentation is not supported for recurrent policies.")
+            # Store symmetry configuration
+            self.symmetry = symmetry_cfg
+        else:
+            self.symmetry = None
+
+        # PPO components
+        self.policy = policy
+        self.policy.to(self.device)
+
+        # Create optimizer
+        self.optimizer = optim.Adam(self.policy.parameters(), lr=learning_rate)
+
+        # Create rollout storage
+        self.storage: RolloutStorage | None = None
+        self.transition = Transition()
+
+        # PPO parameters
+        self.clip_param = clip_param
+        self.num_learning_epochs = num_learning_epochs
+        self.num_mini_batches = num_mini_batches
+        self.value_loss_coef = value_loss_coef
+        self.entropy_coef = entropy_coef
+        self.latent_coef = latent_coef
+        self.gamma = gamma
+        self.lam = lam
+        self.max_grad_norm = max_grad_norm
+        self.use_clipped_value_loss = use_clipped_value_loss
+        self.desired_kl = desired_kl
+        self.schedule = schedule
+        self.learning_rate = learning_rate
+        self.normalize_advantage_per_mini_batch = normalize_advantage_per_mini_batch
+
+        # Latent cloning parameters
+        # Initialize the loss function
+        loss_fn_dict = {
+            "mse": nn.functional.mse_loss,
+            "huber": nn.functional.huber_loss,
+            "bce": nn.functional.binary_cross_entropy,
+        }
+        if loss_type in loss_fn_dict:
+            self.loss_fn = loss_fn_dict[loss_type]
+            self.latent_loss_fn = loss_fn_dict[loss_type]
+            self.confidence_loss_fn = loss_fn_dict["bce"]
+        else:
+            raise ValueError(f"Unknown loss type: {loss_type}. Supported types are: {list(loss_fn_dict.keys())}")
+
+    def init_storage(
+        self,
+        training_type: str,
+        num_envs: int,
+        num_transitions_per_env: int,
+        obs: TensorDict,
+        actions_shape: tuple[int] | list[int],
+    ) -> None:
+        # Create rollout storage
+        self.storage = RolloutStorage(
+            training_type,
+            num_envs,
+            num_transitions_per_env,
+            obs,
+            actions_shape,
+            self.device,
+        )
+
+    def act(self, obs: TensorDict) -> torch.Tensor:
+        if self.policy.is_recurrent:
+            self.transition.hidden_states = self.policy.get_hidden_states()
+        # Compute the actions and values
+        self.transition.latent = self.policy.get_latent(obs).detach()
+        self.transition.actions = self.policy.act(obs).detach()
+        self.transition.values = self.policy.evaluate(obs).detach()
+        self.transition.actions_log_prob = self.policy.get_actions_log_prob(self.transition.actions).detach()
+        self.transition.action_mean = self.policy.action_mean.detach()
+        self.transition.action_sigma = self.policy.action_std.detach()
+        # Record observations before env.step()
+        self.transition.observations = obs
+        return self.transition.actions
+
+    def process_env_step(
+        self, obs: TensorDict, rewards: torch.Tensor, dones: torch.Tensor, extras: dict[str, torch.Tensor]
+    ) -> None:
+        # Update the normalizers
+        self.policy.update_normalization(obs)
+        if self.rnd:
+            self.rnd.update_normalization(obs)
+
+        # Record the rewards and dones
+        # Note: We clone here because later on we bootstrap the rewards based on timeouts
+        self.transition.rewards = rewards.clone()
+        self.transition.dones = dones
+
+        # Compute the intrinsic rewards and add to extrinsic rewards
+        if self.rnd:
+            # Compute the intrinsic rewards
+            self.intrinsic_rewards = self.rnd.get_intrinsic_reward(obs)
+            # Add intrinsic rewards to extrinsic rewards
+            self.transition.rewards += self.intrinsic_rewards
+
+        # Bootstrapping on time outs
+        if "time_outs" in extras:
+            self.transition.rewards += self.gamma * torch.squeeze(
+                self.transition.values * extras["time_outs"].unsqueeze(1).to(self.device), 1
+            )
+
+        # Record the transition
+        self.storage.add_transitions(self.transition)
+        self.transition.clear()
+        self.policy.reset(dones)
+
+    def compute_returns(self, obs: TensorDict) -> None:
+        # Compute value for the last step
+        last_values = self.policy.evaluate(obs).detach()
+        self.storage.compute_returns(
+            last_values, self.gamma, self.lam, normalize_advantage=not self.normalize_advantage_per_mini_batch
+        )
+
+    def update(self) -> dict[str, float]:
+        mean_latent_loss = 0
+        mean_confidence_loss = 0
+       
+        # Get mini batch generator
+        if self.policy.is_recurrent:
+            generator = self.storage.recurrent_mini_batch_generator(self.num_mini_batches, self.num_learning_epochs)
+        else:
+            generator = self.storage.mini_batch_generator(self.num_mini_batches, self.num_learning_epochs)
+
+        # Iterate over batches
+        for (
+            obs_batch,
+            actions_batch,
+            target_values_batch,
+            advantages_batch,
+            returns_batch,
+            old_actions_log_prob_batch,
+            old_mu_batch,
+            old_sigma_batch,
+            hidden_states_batch,
+            masks_batch,
+        ) in generator:
+            num_aug = 1  # Number of augmentations per sample. Starts at 1 for no augmentation.
+            original_batch_size = obs_batch.batch_size[0]
+
+            # Check if we should normalize advantages per mini batch
+            if self.normalize_advantage_per_mini_batch:
+                with torch.no_grad():
+                    advantages_batch = (advantages_batch - advantages_batch.mean()) / (advantages_batch.std() + 1e-8)
+
+            # Compute latent loss
+            latent = self.policy.get_latent(obs_batch)
+            latent_loss = self.latent_loss_fn(self.policy.get_critic_obs(obs_batch)[:, 3:6], 
+                                              latent[:, :3], 
+                                              weight=self.policy.get_critic_obs(obs_batch)[:, 6].unsqueeze(1).repeat(1, 3))
+
+            confidence = latent[:, 3]
+            confidence_loss = self.confidence_loss_fn(confidence, self.policy.get_critic_obs(obs_batch)[:, 6])
+            
+            loss = self.latent_coef * latent_loss + confidence_loss 
+
+            self.optimizer.zero_grad()
+            loss.backward()
+
+            # Collect gradients from all GPUs
+            if self.is_multi_gpu:
+                self.reduce_parameters()
+
+            # Apply the gradients for PPO
+            nn.utils.clip_grad_norm_(self.policy.parameters(), self.max_grad_norm)
+            self.optimizer.step()
+            # Apply the gradients for RND
+            if self.rnd_optimizer:
+                self.rnd_optimizer.step()
+
+            
+            # Store the losses
+            mean_latent_loss += latent_loss.item()
+            mean_confidence_loss += confidence_loss.item()
+
+        # Divide the losses by the number of updates
+        num_updates = self.num_learning_epochs * self.num_mini_batches
+        mean_latent_loss /= num_updates
+        mean_confidence_loss /= num_updates
+
+        # Clear the storage
+        self.storage.clear()
+
+        # Construct the loss dictionary
+        loss_dict = {
+            "latent": mean_latent_loss,
+            "confidence": mean_confidence_loss,
+        }
+
+        return loss_dict
+
+    def broadcast_parameters(self) -> None:
+        """Broadcast model parameters to all GPUs."""
+        # Obtain the model parameters on current GPU
+        model_params = [self.policy.state_dict()]
+        if self.rnd:
+            model_params.append(self.rnd.predictor.state_dict())
+        # Broadcast the model parameters
+        torch.distributed.broadcast_object_list(model_params, src=0)
+        # Load the model parameters on all GPUs from source GPU
+        self.policy.load_state_dict(model_params[0])
+        if self.rnd:
+            self.rnd.predictor.load_state_dict(model_params[1])
+
+    def reduce_parameters(self) -> None:
+        """Collect gradients from all GPUs and average them.
+
+        This function is called after the backward pass to synchronize the gradients across all GPUs.
+        """
+        # Create a tensor to store the gradients
+        grads = [param.grad.view(-1) for param in self.policy.parameters() if param.grad is not None]
+        if self.rnd:
+            grads += [param.grad.view(-1) for param in self.rnd.parameters() if param.grad is not None]
+        all_grads = torch.cat(grads)
+
+        # Average the gradients across all GPUs
+        torch.distributed.all_reduce(all_grads, op=torch.distributed.ReduceOp.SUM)
+        all_grads /= self.gpu_world_size
+
+        # Get all parameters
+        all_params = self.policy.parameters()
+        if self.rnd:
+            all_params = chain(all_params, self.rnd.parameters())
+
+        # Update the gradients for all parameters with the reduced gradients
+        offset = 0
+        for param in all_params:
+            if param.grad is not None:
+                numel = param.numel()
+                # Copy data back from shared buffer
+                param.grad.data.copy_(all_grads[offset : offset + numel].view_as(param.grad.data))
+                # Update the offset for the next parameter
+                offset += numel
